ed6e4d5dc4bfb79d2f905c7484cb9af92a004be8
IanoSang <ianosang18@gmail.com>
2022-02-23 11:15:38 +0300

    feat- login page- adds a user login page to the system

---
+++ login.php
@@ -0,0 +1,157 @@
+<?php
+session_start();
+error_reporting(0);
+include("dbconnection.php");
+if(isset($_POST['login']))
+{
+$ret=mysqli_query($con,"SELECT * FROM user WHERE email='".$_POST['email']."' and password='".$_POST['password']."'");
+$num=mysqli_fetch_array($ret);
+if($num>0)
+{
+$_SESSION['login']=$_POST['email'];
+$_SESSION['id']=$num['id'];
+$_SESSION['name']=$num['name'];
+$val3 =date("Y/m/d");
+date_default_timezone_set("Asia/Calcutta");
+$time=date("h:i:sa");
+$tim = $time;
+$ip_address=$_SERVER['REMOTE_ADDR'];
+$geopluginURL='http://www.geoplugin.net/php.gp?ip='.$ip_address;
+$addrDetailsArr = unserialize(file_get_contents($geopluginURL)); 
+$city = $addrDetailsArr['geoplugin_city']; 
+$country = $addrDetailsArr['geoplugin_countryName'];
+ob_start();
+system('ipconfig /all');
+$mycom=ob_get_contents();
+ob_clean();
+$findme = "Physical";
+$pmac = strpos($mycom, $findme);
+$mac=substr($mycom,($pmac+36),17);
+$ret=mysqli_query($con,"insert into usercheck(logindate,logintime,user_id,username,email,ip,mac,city,country)values('".$val3."','".$tim."','".$_SESSION['id']."','".$_SESSION['name']."','".$_SESSION['login']."','$ip_address','$mac','$city','$country')");
+
+$extra="dashboard.php";
+echo "<script>window.location.href='".$extra."'</script>";
+exit();
+}
+else
+{
+$_SESSION['action1']="Invalid username or password";
+$extra="login.php";
+
+echo "<script>window.location.href='".$extra."'</script>";
+exit();
+}
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<meta charset="utf-8" />
+<title>CRM | Login</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta content="" name="description" />
+<meta content="" name="author" />
+<link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" media="screen"/>
+<link href="assets/plugins/boostrapv3/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/boostrapv3/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+<link href="assets/css/animate.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/css/style.css" rel="stylesheet" type="text/css"/>
+<link href="assets/css/responsive.css" rel="stylesheet" type="text/css"/>
+<link href="assets/css/custom-icon-set.css" rel="stylesheet" type="text/css"/>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="simple-line-icons/css/simple-line-icons.css">
+    <link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+    <link href="css/jcarousel.css" rel="stylesheet" />
+    <link href="js/owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet" />
+
+</head>
+<body class="error-body no-top">
+<!-- start header -->
+<header>
+    <div class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php"><img src="img/BORMER%20Consult%20Logo%20(1).png" alt="logo"/></a>
+            </div>
+            <div class="navbar-collapse collapse ">
+                <ul class="nav navbar-nav">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="services.php">Services</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li class="active"><a href="login.php">Login</a></li>
+                    <li><a href="registration.php">Sign Up</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header><!-- end header -->
+<div class="container">
+
+  <div class="row login-container column-seperation">  
+        <div class="col-md-5 col-md-offset-1">
+          <h2>Login to Bormer CRM</h2>
+          <p>
+            <a href="registration.php">Sign up Now!</a> if you do not have a account with Us.</p>
+          <br>
+
+		   
+        </div>
+        <div class="col-md-5 "> <br>
+             <p style="color:#F00"><?php echo $_SESSION['action1'];?><?php echo $_SESSION['action1']="";?></p>
+		 <form id="login-form" class="login-form" action="" method="post">
+		 <div class="row">
+		 <div class="form-group col-md-10">
+            <label class="form-label">Email</label>
+            <div class="controls">
+				<div class="input-with-icon  right">                                       
+					<i class=""></i>
+					<input type="email" name="email" id="txtusername" class="form-control" required="true">                                 
+				</div>
+            </div>
+          </div>
+          </div>
+		  <div class="row">
+          <div class="form-group col-md-10">
+            <label class="form-label">Password</label>
+            <span class="help"></span>
+            <div class="controls">
+				<div class="input-with-icon  right">                                       
+					<i class=""></i>
+					<input type="password" name="password" id="txtpassword" class="form-control" required="true">                                 
+				</div>
+            </div>
+          </div>
+          </div>
+		  <div class="row">
+          <div class="control-group  col-md-10">
+            <div class="checkbox checkbox check-success"> <a href="forgot-password.php">Forgot Password </a>&nbsp;&nbsp;
+         </div>
+          </div>
+          </div>
+          <div class="row">
+            <div class="col-md-10">
+              <button class="btn btn-primary btn-cons pull-right" name="login" type="submit">Login</button>
+            </div>
+          </div>
+		  </form>
+        </div>
+     
+    
+  </div>
+</div>
+<script src="assets/plugins/jquery-1.8.3.min.js" type="text/javascript"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="assets/plugins/pace/pace.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+<script src="assets/js/login.js" type="text/javascript"></script>
+</body>
+</html>
