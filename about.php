d55857d12c4a165696977bcbb009794a5c0b19a3
IanoSang <ianosang18@gmail.com>
2022-02-23 10:07:56 +0300

    Feat: About Us-Adds about us page

---
+++ about.php
@@ -0,0 +1,389 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<!-- css -->
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link rel="stylesheet" href="simple-line-icons/css/simple-line-icons.css">
+<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+<link href="css/jcarousel.css" rel="stylesheet" />
+<link href="css/flexslider.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+	<title>About Bormer Consult</title>
+ 
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<div id="wrapper">
+
+	<!-- start header -->
+		<header>
+        <div class="navbar navbar-default navbar-static-top">
+
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="index.php"><img src="img/BORMER%20Consult%20Logo%20(1).png" alt="logo"/></a>
+					</div>
+					<div class="navbar-collapse collapse ">
+						<ul class="nav navbar-nav">
+							<li><a href="index.php">Home</a></li>
+							<li class="active"><a href="about.php">About Us</a></li>
+							<li><a href="services.php">Services</a></li>
+							<li><a href="contact.php">Contact Us</a></li>
+                            <li><a href="login.php">Login</a></li>
+                            <li><a href="registration.php">Sign Up</a></li>
+						</ul>
+					</div>
+				</div>
+        </div>
+	</header><!-- end header -->
+	<section id="inner-headline">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h2 class="pageTitle">About Us</h2>
+			</div>
+		</div>
+	</div>
+	</section>
+	<section id="content">
+	<div class="container">
+					
+					<div class="about">
+				
+						<section class="features">
+        <div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div><h2>Who We Are</h2>Bormer Consult is a Kenyan privately owned company founded in 2018 and
+					headquartered in Nairobi. Bormer Consult  assists corporate clients and parties with operations and installation in Kenya.
+					Bormer Consult is established as a consultancy to exclusively provide immigration services.
+					Our highly professional approach, combined with our dedication to technical excellence, result in effective support for immigration requests and application processing under tight time constraints.
+				</div>
+				<br>
+			</div>
+		</div>
+		  	<div class="row">
+			<div class="col-md-12">
+				<div><h2> How we work with you </h2></div>
+				<br/>
+			</div>
+		</div>
+          <div class="row">         
+            <div class="col-md-6">
+              <div class="features-item">
+                <div class="features">
+                  <div class="icon">
+                   <i class="icon-map icons"></i>
+                  </div>
+                  <div class="features-content">
+                    <h3>You tell us your preference </h3>
+                    <p>Via our contacts or a one on one meeting, we discuss the scope of work and deliverables before we propose a solution.</p>
+                  </div>
+                </div>
+                <div class="features">
+                  <div class="icon">
+                    <i class="icon-envelope-open icons"></i>
+                  </div>
+                  <div class="features-content">
+                    <h3>We do the hard work</h3>
+                    <p>We work with you to design tailored solutions that meet the goals with respect to your organization’s culture, disclosure and budget.</p>
+                  </div>
+                </div>
+                <div class="features">
+                  <div class="icon">
+                    <i class="icon-badge icons"></i>
+                  </div>
+                  <div class="features-content">
+                    <h3>You get piece of mind</h3>
+                    <p>When we partner with you, our sustainable solutions ensure that you are equipped with appropriate skills and resources to continue developing. We hold ourselves to high
+						standards of performance and services. Therefore,we welcome your feedback and any stage of our consulting engagements.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <img class="img-responsive" src="img/img1.png" alt="">
+            </div>
+          </div>
+        </div>
+      </section>
+						<div class="row">
+							<div class="col-md-4">
+								<!-- Heading and para -->
+								<div class="block-heading-two">
+									<h3><span>Why Choose Us?</span></h3>
+								</div>
+								<p>We make a complex process easy for you from start to finish.<br/>
+									We guide on requirements and procedures of applying for Kenya immigration residence status.
+									<br/>We assist in preparation and submission of these applications to the immigration department, monitor and report/update on their progress in Kenya.
+									<br/>We establish a good report with our clients and the department of immigration to ensure seamless operations and high standard service delivery.
+
+									</p>
+							</div>
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Our Principles</span></h3>
+								</div>		
+								<!-- Accordion starts -->
+								<div class="panel-group" id="accordion-alt3">
+								 <!-- Panel. Use "panel-XXX" class for different colors. Replace "XXX" with color. -->
+								  <div class="bg-danger">
+									<!-- Panel heading -->
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+											<i class="fa fa-angle-right"></i> Our Vision
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseOne-alt3" class="panel-collapse collapse">
+										<!-- Panel body -->
+										<div class="panel-body">
+											<p>Transcend as a global leading professional service firm focused on best practice consulting and Immigration and relocation services.
+											</p>
+										</div>
+									 </div>
+								  </div>
+								  <div class="bg-success">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
+											<i class="fa fa-angle-right"></i> Our Mission
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseTwo-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+											To deliver effective tailored immigration and relocation solutions to individuals and corporate clients while building business excellence and lasting
+											relationships through constant innovation and progressive growth.
+										</div>
+									 </div>
+								  </div>
+								  <div class="bg-warning">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
+											<i class="fa fa-angle-right"></i> Core values:
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseThree-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+											Everything we undertake is promised on integrity, professionalism, transparency, reliability and efficiency. (commitment, expertise, quality). Highest ethical standards.
+										</div>
+									 </div>
+								  </div>
+									<div class="bg-warning">
+										<div class="panel-heading">
+											<h4 class="panel-title">
+												<a data-toggle="collapse" data-parent="#accordion-alt4" href="#collapseThree-alt4">
+													<i class="fa fa-angle-right"></i> Confidentiality
+												</a>
+											</h4>
+										</div>
+										<div id="collapseThree-alt4" class="panel-collapse collapse">
+											<div class="panel-body">
+												In all respects of our practice, we maintain strict confidentiality towards
+												information obtained while carrying out our professional duties.
+											</div>
+										</div>
+									</div>
+								</div>
+								<!-- Accordion ends -->
+
+							</div>
+
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Our Expertise</span></h3>
+								</div>
+								<h6>Business registration</h6>
+								<div class="progress pb-sm">
+									<!-- White color (progress-bar-white) -->
+									<div class="progress-bar progress-bar-blue" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+										<span class="sr-only">40% Complete (success)</span>
+									</div>
+								</div>
+								<h6>Immigration and Compliance with internal obligations</h6>
+								<div class="progress pb-sm">
+									<div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+										<span class="sr-only">40% Complete (success)</span>
+									</div>
+								</div>
+								<h6>Office and home space</h6>
+								<div class="progress pb-sm">
+									<div class="progress-bar progress-bar-red" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+										<span class="sr-only">40% Complete (success)</span>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+
+
+						<br>
+						<!-- Our Team starts -->
+				
+						<!-- Heading -->
+<!--						<div class="block-heading-six">-->
+<!--							<h4 class="bg-color">Our Team</h4>-->
+<!--						</div>-->
+<!--						<br>-->
+						
+						<!-- Our team starts -->
+						
+<!--						<div class="team-six">-->
+<!--							<div class="row">-->
+<!--								<div class="col-md-3 col-sm-6">-->
+<!--									&lt;!&ndash; Team Member &ndash;&gt;-->
+<!--									<div class="team-member">-->
+<!--										&lt;!&ndash; Image &ndash;&gt;-->
+<!--										<img class="img-responsive" src="img/team1.jpg" alt="">-->
+<!--										&lt;!&ndash; Name &ndash;&gt;-->
+<!--										<h4>Johne Doe</h4>-->
+<!--										<span class="deg">Creative</span> -->
+<!--									</div>-->
+<!--								</div>-->
+<!--								<div class="col-md-3 col-sm-6">-->
+<!--									&lt;!&ndash; Team Member &ndash;&gt;-->
+<!--									<div class="team-member">-->
+<!--										&lt;!&ndash; Image &ndash;&gt;-->
+<!--										<img class="img-responsive" src="img/team2.jpg" alt="">-->
+<!--										&lt;!&ndash; Name &ndash;&gt;-->
+<!--										<h4>Jennifer</h4>-->
+<!--										<span class="deg">Programmer</span> -->
+<!--									</div>-->
+<!--								</div>-->
+<!--								<div class="col-md-3 col-sm-6">-->
+<!--									&lt;!&ndash; Team Member &ndash;&gt;-->
+<!--									<div class="team-member">-->
+<!--										&lt;!&ndash; Image &ndash;&gt;-->
+<!--										<img class="img-responsive" src="img/team3.jpg" alt="">-->
+<!--										&lt;!&ndash; Name &ndash;&gt;-->
+<!--										<h4>Mercy Bor</h4>-->
+<!--										<span class="deg">Founder & CEO</span>-->
+<!--									</div>-->
+<!--								</div>-->
+<!--								<div class="col-md-3 col-sm-6">-->
+<!--									&lt;!&ndash; Team Member &ndash;&gt;-->
+<!--									<div class="team-member">-->
+<!--										&lt;!&ndash; Image &ndash;&gt;-->
+<!--										<img class="img-responsive" src="img/team4.jpg" alt="">-->
+<!--										&lt;!&ndash; Name &ndash;&gt;-->
+<!--										<h4>Ian Sang</h4>-->
+<!--										<span class="deg">IT Manager</span>-->
+<!--									</div>-->
+<!--								</div>-->
+<!--							</div>-->
+<!--						</div>-->
+<!--						&lt;!&ndash; Our team ends &ndash;&gt;-->
+
+
+
+	  </section>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<div class="widget">
+						<h5 class="widgetheading">Company Information</h5>
+						<address>
+							<strong>Bormer Consult</strong><br>
+							Landmark Plaza
+							<br>
+							Upper Hill,<br>
+							Argwings Kodhek Road,<br>
+							Nairobi, KENYA
+						</address>
+						Contact: Mercy Chebet <br>
+						<i class="icon-phone"></i> Cell +254 728 962 888 <br>
+						<i class="icon-envelope-alt"></i> bormerconsult@gmail.com
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="widget">
+						<h5 class="widgetheading">Quick Links</h5>
+						<ul class="link-list">
+							<li><a href="services.php">Our Services</a></li>
+							<li><a href="#">Terms and conditions</a></li>
+							<li><a href="#">Privacy policy</a></li>
+							<li><a href="#">Career</a></li>
+							<li><a href="contact.php">Contact us</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="widget">
+						<h5 class="widgetheading">Latest posts</h5>
+						<ul class="link-list">
+							<li><a href="https://immigration.go.ke/">Directorate of Immigration Services</a></li>
+							<li><a href="https://evisa.go.ke/evisa.html">Republic of Kenya Electronic Visa System</a></li>
+							<li><a href="https://fns.immigration.go.ke/">Kenya Foreign Nationals Service Portal</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="widget">
+						<h5 class="widgetheading">Recent News</h5>
+						<ul class="link-list">
+							<li><a href="https://www.magicalkenya.com/mkse/">Magical Kenya Official Website</a></li>
+							<li><a href="https://tembeakenya.co.ke/">Tembea Kenya Blog</a></li>
+							<li><a href="https://ktb.go.ke/">Kenya Tourism Board Website</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="sub-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="copyright">
+							<p>
+								<span>&copy; Bormer Consult 2022, All rights reserved. </span>
+							</p>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<ul class="social-network">
+							<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+							<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+							<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</div>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+<!-- javascript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.fancybox-media.js"></script> 
+<script src="js/portfolio/jquery.quicksand.js"></script>
+<script src="js/portfolio/setting.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/animate.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
