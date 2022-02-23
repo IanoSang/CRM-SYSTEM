b6266b1035f2171b20ab2bed78e808fcf84be112
IanoSang <ianosang18@gmail.com>
2022-02-23 11:30:51 +0300

    feat: scss services page- adds a page thats deplays bormer services

---
+++ services.php
@@ -0,0 +1,457 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+	<title>Bormer Consult Services</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+ 
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+<link href="css/jcarousel.css" rel="stylesheet" />
+<link href="css/flexslider.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+
+ 
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<div id="wrapper">
+
+	<!-- start header -->
+		<header>
+        <div class="navbar navbar-default navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index.php"><img src="img/BORMER%20Consult%20Logo%20(1).png" alt="logo"/></a>
+				</div>
+				<div class="navbar-collapse collapse ">
+					<ul class="nav navbar-nav">
+						<li ><a href="index.php">Home</a></li>
+						<li><a href="about.php">About Us</a></li>
+						<li class="active"><a href="services.php">Services</a></li>
+						<li><a href="contact.php">Contact Us</a></li>
+                        <li><a href="login.php">Login</a></li>
+                        <li><a href="registration.php">Sign Up</a></li>
+					</ul>
+				</div>
+			</div>
+        </div>
+	</header><!-- end header -->
+	<section id="inner-headline">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h2 class="pageTitle">Services</h2>
+			</div>
+		</div>
+	</div>
+	</section>
+	<section id="content">
+		<div class="container content">	
+<div class="row">
+			<div class="col-md-12 text-center">
+				<div><h2>We offer the below immigration and relocation services.</h2>
+				</div>
+				<br>
+			</div>
+		</div>		
+        <!-- Service Blcoks -->
+			<div class="row">
+				<div class="col-sm-4 info-blocks">
+					<img src="img/Work-Permit.png" class="img-responsive" alt="...">
+					<div class="info-blocks-in">
+						<div class="panel-heading">
+							<h3 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+									<i class="fa fa-angle-right"></i> Work Permit
+								</a>
+							</h3>
+						</div>
+						<div id="collapseOne-alt3" class="panel-collapse collapse">
+							<div class="panel-body">
+								This is a document issued by the Director of Immigration Services, under the provisions of
+								section 40 of the Kenya Citizenship and Immigration Act, 2011, to enable foreign national (s) enter
+								Kenya and engage in trade, prospecting, farming, business, professional employment, missionary activities
+								or even reside in Kenya.
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-4 info-blocks">
+					<img class="thumbnail img-responsive"  src="img/Dependent-Pass.jpg"
+						 style="maxwidth: 400px;height: auto;max-height: 230px;">
+					<div class="info-blocks-in">
+						<div class="panel-heading">
+							<h3 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
+									<i class="fa fa-angle-right"></i> Dependent Pass
+								</a>
+							</h3>
+						</div>
+						<div id="collapseTwo-alt3" class="panel-collapse collapse">
+							<div class="panel-body">
+								A dependent pass is issued to a spouse, parent or guardian of a foreign national who is lawfully allowed to enter for work purposes or other purposes. The validity of the dependent pass is granted
+								at the discretion of the immigration officer and is often tied to the validity of the main applicant’s permit or pass.
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-4 info-blocks">
+					<img class="thumbnail img-responsive"  src="img/Pass.png"
+						 style="maxwidth: 600px;height: auto;max-height: 230px;">
+					<div class="info-blocks-in">
+						<div class="panel-heading">
+							<h3 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
+									<i class="fa fa-angle-right"></i> Special Pass
+								</a>
+							</h3>
+						</div>
+						<div id="collapseThree-alt3" class="panel-collapse collapse">
+							<div class="panel-body">
+								Special passes are issued to individuals who would like to
+								conduct any business, trade, or profession or those wishing to apply for another
+								permit in-country. This visa may be granted for up to 90 days.
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-4 info-blocks">
+					<img class="thumbnail img-responsive"  src="img/airport.jpg"
+						 style="maxwidth: 400px;height: auto;max-height: 200px;">
+					<div class="info-blocks-in">
+						<div class="panel-heading">
+							<h3 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFour-alt3">
+									<i class="fa fa-angle-right"></i> Airport Meet and Greet
+								</a>
+							</h3>
+						</div>
+						<div id="collapseFour-alt3" class="panel-collapse collapse">
+							<div class="panel-body">
+								To deliver effective tailored immigration and relocation solutions to individuals and corporate clients while building business excellence and lasting
+								relationships through constant innovation and progressive growth.
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-4 info-blocks">
+					<img class="thumbnail img-responsive"  src="img/permanent.jpg"
+						 style="maxwidth: 400px;height: auto;max-height: 230px;">
+					<div class="info-blocks-in">
+
+						<div class="panel-heading">
+							<h3 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFive-alt3">
+									<i class="fa fa-angle-right"></i> Permanent Residence Permit
+								</a>
+							</h3>
+						</div>
+						<div id="collapseFive-alt3" class="panel-collapse collapse">
+							<div class="panel-body">
+								This applies to those who have held Kenya a work permit for at least seven years and
+								have been residing in the country continuously for the last three years can apply for permanent
+								residence (PR). It is possible for the main applicant's spouse and children to also gain PR at the same time.
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-4 info-blocks">
+					<img class="thumbnail img-responsive"  src="img/kenya.jpg"
+						 style="maxwidth: 400px;height: auto;max-height: 200px;">
+					<div class="info-blocks-in">
+						<div class="panel-heading">
+							<h3 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseSix-alt3">
+									<i class="fa fa-angle-right"></i> Citizenship
+								</a>
+							</h3>
+						</div>
+						<div id="collapseSix-alt3" class="panel-collapse collapse">
+							<div class="panel-body">
+								To deliver effective tailored immigration and relocation solutions to individuals and corporate clients while building business excellence and lasting
+								relationships through constant innovation and progressive growth.
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+        <!-- End Service Blcoks -->
+
+        <!-- Info Blocks -->
+        <div class="row">
+            <div class="col-sm-4 info-blocks">
+				<img class="thumbnail img-responsive"  src="img/student%20pass.jpg"
+					 style="maxwidth: 400px;height: auto;max-height: 230px;">
+                <div class="info-blocks-in">
+					<div class="panel-heading">
+						<h3 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseSeven-alt3">
+								<i class="fa fa-angle-right"></i> Internship Pass/Students Pass/Pupils Pass
+							</a>
+						</h3>
+					</div>
+					<div id="collapseSeven-alt3" class="panel-collapse collapse">
+						<div class="panel-body">
+							To deliver effective tailored immigration and relocation solutions to individuals and corporate clients while building business excellence and lasting
+							relationships through constant innovation and progressive growth.
+						</div>
+					</div>
+                </div>
+            </div>
+            <div class="col-sm-4 info-blocks">
+				<img class="thumbnail img-responsive"  src="img/multiple.png"
+					 style="maxwidth: 400px;height: auto;max-height: 230px;">
+                <div class="info-blocks-in">
+					<div class="panel-heading">
+						<h3 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseEight-alt3">
+								<i class="fa fa-angle-right"></i>  Multiple Journey Visa
+							</a>
+						</h3>
+					</div>
+					<div id="collapseEight-alt3" class="panel-collapse collapse">
+						<div class="panel-body">
+							To deliver effective tailored immigration and relocation solutions to individuals and corporate clients while building business excellence and lasting
+							relationships through constant innovation and progressive growth.
+						</div>
+					</div>
+                </div>
+            </div>
+            <div class="col-sm-4 info-blocks">
+				<img class="thumbnail img-responsive"  src="img/passport.jpg"
+					 style="maxwidth: 600px;height: auto;max-height: 600px;">
+                <div class="info-blocks-in">
+					<div class="panel-heading">
+						<h3 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseNine-alt3">
+								<i class="fa fa-angle-right"></i> Single Journey Visa
+							</a>
+						</h3>
+					</div>
+					<div id="collapseNine-alt3" class="panel-collapse collapse">
+						<div class="panel-body">
+							To deliver effective tailored immigration and relocation solutions to individuals and corporate clients while building business excellence and lasting
+							relationships through constant innovation and progressive growth.
+						</div>
+					</div>
+                </div>
+            </div>
+        </div>
+        <!-- End Info Blocks -->
+
+			<!-- Info Blcoks -->
+			<div class="row">
+				<div class="col-sm-4 info-blocks">
+					<img class="thumbnail img-responsive"  src="img/visitorespass.jpg"
+						 style="maxwidth: 400px;height: auto;max-height: 230px;">
+					<div class="info-blocks-in">
+						<div class="panel-heading">
+							<h3 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTen-alt3">
+									<i class="fa fa-angle-right"></i> Visitors Pass
+								</a>
+							</h3>
+						</div>
+						<div id="collapseTen-alt3" class="panel-collapse collapse">
+							<div class="panel-body">
+								To deliver effective tailored immigration and relocation solutions to individuals and corporate clients while building business excellence and lasting
+								relationships through constant innovation and progressive growth.
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-4 info-blocks">
+					<img class="thumbnail img-responsive"  src="img/regestration.jpg"
+						 style="maxwidth: 400px;height: auto;max-height: 230px;">
+					<div class="info-blocks-in">
+						<div class="info-blocks-in">
+							<div class="panel-heading">
+								<h3 class="panel-title">
+									<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseEleven-alt3">
+										<i class="fa fa-angle-right"></i> Alien/Foreign National Registration
+									</a>
+								</h3>
+							</div>
+							<div id="collapseEleven-alt3" class="panel-collapse collapse">
+								<div class="panel-body">
+									To deliver effective tailored immigration and relocation solutions to individuals and corporate clients while building business excellence and lasting
+									relationships through constant innovation and progressive growth.
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-4 info-blocks">
+					<img class="thumbnail img-responsive"  src="img/visitorspass.png"
+						 style="maxwidth: 600px;height: auto;max-height: 230px;">
+					<div class="info-blocks-in">
+
+						<div class="panel-heading">
+							<h3 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwelve-alt3">
+									<i class="fa fa-angle-right"></i> Re-entry
+								</a>
+							</h3>
+						</div>
+						<div id="collapseTwelve-alt3" class="panel-collapse collapse">
+							<div class="panel-body">
+								To deliver effective tailored immigration and relocation solutions to individuals and corporate clients while building business excellence and lasting
+								relationships through constant innovation and progressive growth.
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- End Info Blocks -->
+
+			<!-- Info Blocks -->
+			<div class="row">
+				<div class="col-sm-6 info-blocks">
+					<img class="thumbnail img-responsive"  src="img/endorsement.jfif"
+						 style="maxwidth: 400px;height: auto;max-height: 230px;">
+					<div class="info-blocks-in">
+						<div class="panel-heading">
+							<h3 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThirteen-alt3">
+									<i class="fa fa-angle-right"></i> Endorsement and Transfers
+								</a>
+							</h3>
+						</div>
+						<div id="collapseThirteen-alt3" class="panel-collapse collapse">
+							<div class="panel-body">
+								To deliver effective tailored immigration and relocation solutions to individuals and corporate clients while building business excellence and lasting
+								relationships through constant innovation and progressive growth.
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 info-blocks">
+					<img class="thumbnail img-responsive"  src="img/Pass.png"
+						 style="maxwidth: 400px;height: auto;max-height: 230px;">
+					<div class="info-blocks-in">
+						<div class="panel-heading">
+							<h3 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFourteen-alt3">
+									<i class="fa fa-angle-right"></i> Security Bond
+								</a>
+							</h3>
+						</div>
+						<div id="collapseFourteen-alt3" class="panel-collapse collapse">
+							<div class="panel-body">
+								To deliver effective tailored immigration and relocation solutions to individuals and corporate clients while building business excellence and lasting
+								relationships through constant innovation and progressive growth.
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</section>
+
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3">
+				<div class="widget">
+					<h5 class="widgetheading">Company Information</h5>
+					<address>
+						<strong>Bormer Consult</strong><br>
+						Landmark Plaza
+						<br>
+						Upper Hill,<br>
+						Argwings Kodhek Road,<br>
+						Nairobi, KENYA
+					</address>
+					Contact: Mercy Chebet <br>
+					<i class="icon-phone"></i> Cell +254 728 962 888 <br>
+					<i class="icon-envelope-alt"></i> bormerconsult@gmail.com
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="widget">
+					<h5 class="widgetheading">Quick Links</h5>
+					<ul class="link-list">
+						<li><a href="services.php">Our Services</a></li>
+						<li><a href="#">Terms and conditions</a></li>
+						<li><a href="#">Privacy policy</a></li>
+						<li><a href="#">Career</a></li>
+						<li><a href="contact.php">Contact us</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="widget">
+					<h5 class="widgetheading">Latest posts</h5>
+					<ul class="link-list">
+						<li><a href="https://immigration.go.ke/">Directorate of Immigration Services</a></li>
+						<li><a href="https://evisa.go.ke/evisa.html">Republic of Kenya Electronic Visa System</a></li>
+						<li><a href="https://fns.immigration.go.ke/">Kenya Foreign Nationals Service Portal</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="widget">
+					<h5 class="widgetheading">Recent News</h5>
+					<ul class="link-list">
+						<li><a href="https://www.magicalkenya.com/mkse/">Magical Kenya Official Website</a></li>
+						<li><a href="https://tembeakenya.co.ke/">Tembea Kenya Blog</a></li>
+						<li><a href="https://ktb.go.ke/">Kenya Tourism Board Website</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="sub-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="copyright">
+						<p>
+							<span>&copy; Bormer Consult 2022, All rights reserved. </span>
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<ul class="social-network">
+						<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+						<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+</div>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+<!-- javascript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.fancybox-media.js"></script> 
+<script src="js/portfolio/jquery.quicksand.js"></script>
+<script src="js/portfolio/setting.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/animate.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
