6bdb1f1a0d58d2a4a1d644d2efb2315630b9d7e4
IanoSang <ianosang18@gmail.com>
2022-02-23 11:10:04 +0300

    Feat: Home page- adds the sytem home page

---
+++ index.php
@@ -0,0 +1,328 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="simple-line-icons/css/simple-line-icons.css">
+    <link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+    <link href="css/jcarousel.css" rel="stylesheet" />
+    <link href="css/flexslider.css" rel="stylesheet" />
+    <link href="js/owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet" />
+    <title>Bormer Consult</title>
+</head>
+
+<body>
+<div id="wrapper" class="home-page">
+    <!-- start header -->
+    <header>
+        <div class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php"><img src="img/BORMER%20Consult%20Logo%20(1).png" alt="logo"/></a>
+                </div>
+                <div class="navbar-collapse collapse ">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="services.php">Services</a></li>
+                        <li><a href="contact.php">Contact Us</a></li>
+                        <li><a href="login.php">Login</a></li>
+                        <li><a href="registration.php">Sign Up</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- end header -->
+
+    <section id="banner">
+
+        <!-- Slider -->
+        <div id="main-slider" class="flexslider">
+            <ul class="slides">
+                <li>
+                    <img src="img/slides/1.jpg" alt="" />
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="flex-caption">
+                                    <h3>Immigration Services</h3>
+                                    <p>We make a complex process easy for you from start to finish.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <img src="img/slides/2.jpg" alt="" />
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="flex-caption">
+                                    <h3>Relocation Services</h3>
+                                    <p>We assist corporate clients and parties with operations and installation in Kenya.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <!-- end slider -->
+
+    </section>
+    <section class="callaction">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-center"><h1 >EXECUTIVE SUMMARY</h1>Bormer Consult seeks to continue compliance,
+                        transparency and assignee satisfaction across the corporate immigration in Kenya & Africa .
+                        To achieve these objectives, we believe your chosen provider must be able to deliver:
+                        <p>
+                            <ul>
+                            <li>Proactive and clear communications to enable seamless connectivity;</li>
+                            <li>The highest levels of service quality for your immigration beneficiaries.</li>
+                            <li>We are highly qualified and experienced. </li>
+                            <li>We offer competitive fees with complete transparency, no hidden costs;</li>
+                            <li>We have experience to deliver our service in your key destinations;</li>
+                            <li>We offer a seamless transition;</li>
+                           </ul>
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <span class="text-center text-info"><h1>Our Services</h1></span>
+                    <div class="col-lg-7 pt-5">
+                        <h3 class="my-3">We consult and guide on the requirements and procedures of applying for Kenya
+                            immigration residence status including:-</h3>
+                        <div class="row text-inverse">
+                            <div class="col-md-4 mt-3">
+                                <ul class="list-style-none pl-0">
+                                    <li class="my-2"><span>Work Permit</span></li>
+                                    <li class="my-2"> <span>Special Pass</span></li>
+                                    <li class="my-2"><span>Dependent Pass</span></li>
+                                    <li class="my-2"><span>Re-Entry</span></li>
+                                    <li class="my-2"><span>Internship Pass</span></li>
+
+                                </ul>
+                            </div>
+                            <div class="col-md-4 mt-0 mt-md-3">
+                                <ul class="list-style-none pl-0">
+                                    <li class="my-2"><span>Citizenship</span></li>
+                                    <li class="my-2"> <span>Permanent Residence</span></li>
+                                    <li class="my-2"><span>Student/Pupil Pass</span></li>
+                                    <li class="my-2"><span>Alien Registration</span></li>
+                                    <li class="my-2"><span>Visa extension</span></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-4 mt-0 mt-md-3">
+                                <ul class="list-style-none pl-0">
+
+                                    <li class="my-2"> <span>KRA PIN</span></li>
+                                    <li class="my-2"><span>NHIF</span></li>
+                                    <li class="my-2"><span>NSSF</span></li>
+                                    <li class="my-2"><span>Office and home space</span></li>
+                                    <li class="my-2"><span>Multiple/Single Journey Visa</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <h6 class="subtitle font-weight-light">We also assist in preparation and submission of applications the ministry of
+                            immigration department, monitor and update on their progress.</h6>
+                        <h6 class="subtitle font-weight-light">We establish a good rapport with our clients and the department of immigration
+                            to ensure seamless operations and high standard service delivery.</h6>
+                        <div class="row">
+                            <div class="col-md-12 my-4">
+                                <a class="btn btn-info-gradiant btn-md border-0 text-white" href="services.php"><span>More Details</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 text-center">
+                        <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/features/feature19/img1.jpg" alt="wrappixel">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- row -->
+
+    </section>
+    <section class="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="text-center"><h2>The Bormer Advantages</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="features-item">
+                        <div class="features">
+                            <div class="icon">
+                                <i class="icon-map icons"></i>
+                            </div>
+                            <div class="features-content">
+                                <h3>Extensive Experience</h3>
+                                <p>Enjoy our expertise and experience of over five years of practice in immigration
+                                    consultancy, immigration guidance and process management.</p>
+                            </div>
+                        </div>
+                        <div class="features">
+                            <div class="icon">
+                                <i class="icon-envelope-open icons"></i>
+                            </div>
+                            <div class="features-content">
+                                <h3>Top Notch Partners</h3>
+                                <p>Benefit from our partnerships with reputable associates and organizations in the
+                                    best interest of our clients.</p>
+                            </div>
+                        </div>
+                        <div class="features">
+                            <div class="icon">
+                                <i class="icon-badge icons"></i>
+                            </div>
+                            <div class="features-content">
+                                <h3>Tailored Services</h3>
+                                <p>We develop customized approaches and offer flexible solutions tailored to your
+                                    specific requirements.</p>
+                            </div>
+                        </div>
+                        <div class="features">
+                            <div class="icon">
+                                <i class="icon-badge icons"></i>
+                            </div>
+                            <div class="features-content">
+                                <h3>Excellent Follow-up Services</h3>
+                                <p>We forge a strong and long-lasting relationship through our dedicated follow-up
+                                    service.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img class="img-responsive" src="img/img1.png" alt="">
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <section class="aboutUs">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6"><img src="img/img1.png" class="img-center" alt="" /></div>
+                <div class="col-md-6">
+                    <div><h2>Our Team</h2>
+                        <p>Bormer Consult will enhance the current account management team with members of our relocation & Visa & Immigration team who have the knowledge and experience of managing clients with a similar V&I programme</p>
+                        <p>Our team will customize service delivery to ensure that we provide world-class services with complete cost transparency. </p>
+                    </div>
+                    <br/>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="widget">
+                        <h5 class="widgetheading">Company Information</h5>
+                        <address>
+                            <strong>Bormer Consult</strong><br>
+                            Landmark Plaza
+                            <br>
+                            Upper Hill,<br>
+                            Argwings Kodhek Road,<br>
+                            Nairobi, KENYA
+                        </address>
+                        Contact: Mercy Chebet <br>
+                        <i class="icon-phone"></i> Cell +254 728 962 888 <br>
+                        <i class="icon-envelope-alt"></i> bormerconsult@gmail.com
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="widget">
+                        <h5 class="widgetheading">Quick Links</h5>
+                        <ul class="link-list">
+                            <li><a href="services.php">Our Services</a></li>
+                            <li><a href="#">Terms and conditions</a></li>
+                            <li><a href="#">Privacy policy</a></li>
+                            <li><a href="#">Career</a></li>
+                            <li><a href="contact.php">Contact us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="widget">
+                        <h5 class="widgetheading">Latest posts</h5>
+                        <ul class="link-list">
+                            <li><a href="https://immigration.go.ke/">Directorate of Immigration Services</a></li>
+                            <li><a href="https://evisa.go.ke/evisa.html">Republic of Kenya Electronic Visa System</a></li>
+                            <li><a href="https://fns.immigration.go.ke/">Kenya Foreign Nationals Service Portal</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="widget">
+                        <h5 class="widgetheading">Recent News</h5>
+                        <ul class="link-list">
+                            <li><a href="https://www.magicalkenya.com/mkse/">Magical Kenya Official Website</a></li>
+                            <li><a href="https://tembeakenya.co.ke/">Tembea Kenya Blog</a></li>
+                            <li><a href="https://ktb.go.ke/">Kenya Tourism Board Website</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="sub-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="copyright">
+                            <p>
+                                <span>&copy; Bormer Consult 2022, All rights reserved. </span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <ul class="social-network">
+                            <li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+                            <li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+    <!-- javascript
+        ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.fancybox.pack.js"></script>
+    <script src="js/jquery.fancybox-media.js"></script>
+    <script src="js/portfolio/jquery.quicksand.js"></script>
+    <script src="js/portfolio/setting.js"></script>
+    <script src="js/jquery.flexslider.js"></script>
+    <script src="js/animate.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="js/owl-carousel/owl.carousel.js"></script>
+</div>
+</body>
+</html>
