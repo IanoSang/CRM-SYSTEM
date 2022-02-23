7b80c2b3e87b120034f304209acc6e2f167d9e0e
IanoSang <ianosang18@gmail.com>
2022-02-23 11:01:30 +0300

    Feat: Header page- adds header to the page

---
+++ header.php
@@ -0,0 +1,42 @@
+<div class="header navbar navbar-inverse ">
+  <div class="navbar-inner">
+    <div class="header-seperation">
+      <ul class="nav pull-left notifcation-center" id="main-menu-toggle-wrapper" style="display:none">
+        <li class="dropdown"> <a id="main-menu-toggle" href="#main-menu"  class="" >
+          <div class="iconset top-menu-toggle-white"></div>
+          </a> </li>
+      </ul>
+      <a href="dashboard.php" style="color:#FFF; font-size:24px; margin-top:20%;">CRM</a>
+      <ul class="nav pull-right notifcation-center">
+        <li class="dropdown" id="header_task_bar"> <a href="dashboard.php" class="dropdown-toggle active" data-toggle="">
+          <div class="iconset top-home"></div>
+          </a> </li>
+      
+      </ul>
+    </div>
+    <div class="header-quick-nav" >
+      <div class="pull-left">
+       
+      
+      </div>
+      <div class="pull-right">
+        <ul class="nav quick-section ">
+          <li class="quicklinks"> <a data-toggle="dropdown" class="dropdown-toggle  pull-right " href="#" id="user-options">
+            <div class="iconset top-settings-dark "></div>
+            </a>
+            <ul class="dropdown-menu  pull-right" role="menu" aria-labelledby="user-options">
+              <li><a href="profile.php"> My Account</a> </li>
+              <li class="divider"></li>
+              <li><a href="logout.php"><i class="fa fa-power-off"></i>&nbsp;&nbsp;Log Out</a></li>
+            </ul>
+          </li>
+
+        </ul>
+      </div>
+      <!-- END CHAT TOGGLER -->
+    </div>
+    <!-- END TOP NAVIGATION MENU -->
+  </div>
+  <!-- END TOP NAVIGATION BAR -->
+</div>
+<!-- END HEADER -->
