69cbec0b177016117de197b22fc21ff182016d21
IanoSang <ianosang18@gmail.com>
2022-03-08 16:56:04 +0300

    adds changes to check login funtion

--- checklogin.php
+++ checklogin.php
@@ -2,12 +2,12 @@
 function check_login()
 {
 if(strlen($_SESSION['login'])==0)
-	{	
+	{
 		$host=$_SERVER['HTTP_HOST'];
 		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-		$extra="index.php";		
+		$extra="index.php";
 		$_SESSION["login"]="";
-		header("Location: http://$host$uri/$extra");
+		header("Location: https://$host$uri/$extra");
 	}
 }
 ?>
